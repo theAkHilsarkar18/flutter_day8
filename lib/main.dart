900e646b79c0d941a0dbf02a50dbb0bea4633e59
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2022-12-15 20:11:44 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+import 'index.dart';
+import 'index2.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:
+        //Index2(),
+      Index(),
+    )
+  );
+}
